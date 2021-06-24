6ff4c7488ee24602589660a303566132ba8e6e8b
Sugam Shrestha <sugamsht143@gmail.com>
2021-06-24 21:58:32 +0545

    Flexbox

--- App.js
+++ App.js
@@ -3,23 +3,45 @@ import React from 'react';
 import {
   StyleSheet, Text, View, Platform, SafeAreaView, NativeModules,
 } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
+// import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 const { StatusBarManager } = NativeModules;
 
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  console.log(useDimensions(), useDeviceOrientation());
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={{
+      backgroundColor: 'white',
+      flex: 1,
+      flexDirection: 'row', //aligns horizontally 
+      justifyContent: 'center', //aligns in primary axis
+      alignItems: 'center',  //alignment of content within each line(in secondary axis)
+      // alignContent: 'center', //alignment of entire content
+      // flexWrap: "wrap"
+    }}
+    >
       <View style={{
-        backgroundColor: 'red',
-        width: '100%',
-        height: landscape ? '100%' : '40%'
-      }}>
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+        backgroundColor: 'dodgerblue',
+        width: 300,
+        height: 100,
+        flexShrink: 1, //same as flex:-1
+        // alignSelf: 'flex-start'
+      }} />
+      <View style={{
+        backgroundColor: 'gold',
+        // flexBasis: 100, //sets size along the primary axis
+        // flexGrow: 1,  //same as flex:1
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100,
+        top: 50,
+        position: 'absolute'
+      }} />
+
+    </View>
   );
 }
 
