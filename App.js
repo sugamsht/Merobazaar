927b7e7ce24bf6d4561b1aa82378c1c35a1c6a61
Sugam Shrestha <sugamsht143@gmail.com>
2021-06-24 20:00:47 +0545

    Dimentions and Orientation

--- App.js
+++ App.js
@@ -1,42 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {
-  StyleSheet, Text, View, Platform, SafeAreaView, NativeModules, Image, TouchableWithoutFeedback, TouchableOpacity,
-  TouchableHighlight, TouchableNativeFeedback, Button, Alert,
+  StyleSheet, Text, View, Platform, SafeAreaView, NativeModules,
 } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 const { StatusBarManager } = NativeModules;
-import { ActivityIndicator } from 'react-native';
+
 
 export default function App() {
-  const onclick = () => console.log("dead");
+  const { landscape } = useDeviceOrientation();
+  console.log(useDimensions(), useDeviceOrientation());
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onLongPress={onclick}>Hello React Hello from the other siiiiiide today is euro cup  who will win</Text>
-      <Text>Height is {StatusBarManager.HEIGHT} </Text>
-      <TouchableOpacity>
-        <Image style={styles.image} source={require('./assets/icon.png')} />
-      </TouchableOpacity>
-      <TouchableHighlight onPress={() => { console.log('Don\'t touch the image') }}>
-        <Image
-          blurRadius={0}
-          source={{
-            height: 200,
-            width: 300,
-            uri: "https://picsum.photos/200/300/?blur=1"
-          }} />
-      </TouchableHighlight>
-      <TouchableNativeFeedback onLongPress={() => { console.log('asdh') }}>
-        <View style={{ width: 200, height: 100, backgroundColor: 'red' }}>
-        </View>
-      </TouchableNativeFeedback>
-      <Button
-        color="green"
-        title="Click me"
-        onPress={() => Alert.alert("My title", "My message",
-          [{ text: 'Yes', onPress: () => console.log('You clicked yes') },
-          { text: "No", onPress: () => console.log('You clicked no') }])}
-      // onPress={() => Alert.prompt("My title", "My message", text => console.log(text))} //Only works on IOS
-      />
+      <View style={{
+        backgroundColor: 'red',
+        width: '100%',
+        height: landscape ? '100%' : '40%'
+      }}>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -50,9 +31,5 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? StatusBarManager.HEIGHT : 0,
     alignItems: 'center',
     // justifyContent: 'center',
-  },
-  image: {
-    width: 200,
-    height: 200,
   }
 });
